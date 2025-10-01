0d86bed6df5fb0ab055af0a9597da3f51630fd8b
mateudovicic <mateudovicic406@gmail.com>
2025-10-01 09:55:51 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+#rjecnik
+kosarica={}
+
+#dodavanje elemenata u rječnik
+print(f"Prazna košarica: {kosarica}")
+print(f"Tip: {type(kosarica)}")
+
+kosarica['jabuke'] = 15
+kosarica['kreke'] = 23
+kosarica['mandarini'] = 3
+
+#ispis rječnika
+print(f"Napunjena košarica: {kosarica}")
